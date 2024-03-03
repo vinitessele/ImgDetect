7721d48ec8687cde4af8a122fa4f8e2e41d53704
vinitessele <vinicius_tessele@hotmail.com>
2024-03-03 10:13:22 -0300

    first commit

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import cv2
 
 classificador = cv2.CascadeClassifier(r"D:\ImagensDigitaisPy\exemploDeteccaoImg\cascades\haarcascade_frontalface_default.xml")
-imagem = cv2.imread(r"D:\ImagensDigitaisPy\exemploDeteccaoImg\3.jpg")
+imagem = cv2.imread(r"D:\ImagensDigitaisPy\exemploDeteccaoImg\1.png")
 
 imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
-cv2.imshow("Original",imagem)
-cv2.imshow("cinza",imagemCinza)
+#cv2.imshow("Original",imagem)
+#cv2.imshow("cinza",imagemCinza)
 #cv2.waitKey(2000)
-facesDeectadas = classificador.detectMultiScale(imagemCinza)
+facesDeectadas = classificador.detectMultiScale(imagemCinza, scaleFactor=1.04,minNeighbors=8,minSize=(30,30))
 print(len(facesDeectadas))
 print(facesDeectadas)
 
